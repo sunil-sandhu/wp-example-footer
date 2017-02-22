d9163309aba64674cf1ed70ae253f6fc2fbfc464
Sunil Sandhu <sunil.sandhu87@gmail.com>
2017-02-22 18:23:30 +0000

    footer.php
    Example of a Wordpress footer.php for a theme.

---
+++ footer.php
@@ -0,0 +1,58 @@
+    <hr>
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="https://www.instagram.com/joyofhex/"> <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                                </span> </a>
+                        </li>
+                        <li>
+                            <a href="http://inpala.co"> <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-chrome fa-stack-1x fa-inverse"></i>
+                                </span> </a>
+                        </li>
+                        <li>
+                            <a href="http://www.twitter.com/inpalaco"> <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span> </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyright &copy; <a href="http://inpala.co">Inpala.co</a> 2016</p>
+                </div>
+            </div>
+        </div> 
+
+</footer>
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+    <!-- Theme JavaScript -->
+    <script src="js/clean-blog.min.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90881526-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<?php wp_footer(); ?> 
+
+</body>
+
+</html>
